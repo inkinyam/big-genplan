0f51697ec84d439fadefef49659a78665dcd12c5
Серебренникова Наталья Сергеевна <nserebrennikova@genplanmos.local>
2022-07-19 16:28:52 +0300

    fix: корректировки по дизайн/код ревью

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 
 /*открытие блоков в PROJECT*/
 const favoriteProjectButton = document.querySelector('.links__favorites');
-const allProjectButton     = document.querySelector('.links__all');
-const favoriteProject = document.querySelector('.project__favorites');
-const allProject = document.querySelector('.project__all');
+const allProjectButton      = document.querySelector('.links__all');
+const favoriteProject       = document.querySelector('.project__favorites');
+const allProject            = document.querySelector('.project__all');
 
 favoriteProjectButton.addEventListener('click', ()=> {
   favoriteProject.classList.add('project_open');
@@ -18,6 +18,31 @@ allProjectButton.addEventListener('click', ()=> {
   favoriteProjectButton.classList.remove('links__link_v_active');
   allProjectButton.classList.add('links__link_v_active');
 }) 
+
+
+/*разворачивание доп.меню в хедер*/
+const bigHederMenu   = document.querySelector('.header__big-list');
+const smallHederMenu = document.querySelector('.header__small-list');
+const activator      = document.querySelector('.header__nav-spetial');
+
+const showSmallMenu = () => {
+  bigHederMenu.classList.add('fadeOut');
+  smallHederMenu.classList.add('fadeInRight');
+}
+
+const hideSmallMenu = (evt) => {
+  if (evt.target!=smallHederMenu||activator) {
+    bigHederMenu.classList.toggle('fadeOut');
+    smallHederMenu.classList.toggle('fadeInRight');
+  }
+
+ 
+}
+
+activator.addEventListener('mouseenter', showSmallMenu);
+activator.addEventListener('mouseleave', evt => {
+  hideSmallMenu(evt);
+})
 
 
 /*инициализация слайдера для блока LEAD*/
