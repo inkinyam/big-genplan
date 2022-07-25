ef38257c3946c528e20dcf0a3079a90025dfa30d
Серебренникова Наталья Сергеевна <nserebrennikova@genplanmos.local>
2022-07-25 14:32:24 +0300

    fix: desktop header

--- scripts/main.js
+++ scripts/main.js
@@ -51,3 +51,11 @@ const interviewSlider = new ChiefSlider('.interviews_slider', {
 );
 
 
+const openSearchBarButton = document.querySelector('.header__search');
+const navBar = document.querySelector('.navBar');
+const searchBar = document.querySelector('.header__search-container');
+
+openSearchBarButton.addEventListener('click', () => {
+  navBar.classList.toggle('header_bar_open');
+  searchBar.classList.toggle('header_bar_open');
+})
